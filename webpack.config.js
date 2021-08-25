66aa17e90241048ed22fed95738fc7d0190cb54d
esride-nik <n.koehn@esri.de>
2021-08-25 14:11:22 +0200

    tsconfig and webpack.config removed

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-const extensionsConfig = require('./webpack/webpack-extensions.config');
-
-if (extensionsConfig.length === 0) {
-  console.warn('You have to have at least one widget/theme.');
-  return;
-}
-module.exports = extensionsConfig;
