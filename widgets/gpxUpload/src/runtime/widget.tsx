7a852fa3b1dece2d8d559b9dd97408366c190d60
esride-nik <n.koehn@esri.de>
2021-08-30 18:06:46 +0200

    FileReader

--- widgets/gpxUpload/src/runtime/widget.tsx
+++ widgets/gpxUpload/src/runtime/widget.tsx
@@ -41,6 +41,19 @@ export default function Widget(props: AllWidgetProps<{ Config }>) {
     const onDrop = useCallback((acceptedFiles) => {
         // Do something with the files
         console.log('dropped files', acceptedFiles);
+
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader();
+
+            reader.onabort = () => console.log('file reading was aborted');
+            reader.onerror = () => console.log('file reading has failed');
+            reader.onload = () => {
+                // Do whatever you want with the file contents
+                const binaryStr = reader.result;
+                console.log(binaryStr);
+            };
+            reader.readAsArrayBuffer(file);
+        });
     }, []);
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
