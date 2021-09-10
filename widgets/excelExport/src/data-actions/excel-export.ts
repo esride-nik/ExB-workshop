7a3210d02e0681dd88f7c3e3779b09bb140cd1c9
esride-nik <n.koehn@esri.de>
2021-09-10 22:42:21 +0200

    excel Export ok. issues:double export, hidden cols

--- widgets/excelExport/src/data-actions/excel-export.ts
+++ widgets/excelExport/src/data-actions/excel-export.ts
@@ -5,10 +5,19 @@ export default class ExportJson extends AbstractDataAction {
         return records?.length > 0;
     }
 
+    // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
     async onExecute(dataSource: DataSource, records: DataRecord[]): Promise<boolean> {
-        const features = records.map((r) => (r as any).feature?.attributes);
-        console.log('action: excel-export onExecute', records, features);
-        MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'features', features);
-        return true;
+        if (records.length > 0) {
+            const features = records.map((r) => (r as any).feature?.attributes);
+            console.log('action: excel-export onExecute', records, features);
+            MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'features', features);
+            MutableStoreManager.getInstance().updateStateValue(
+                this.widgetId,
+                'label',
+                records[0].dataSource?.belongToDataSource?.fetchedSchema?.label
+            );
+            return true;
+        }
+        return false;
     }
 }
--- widgets/excelExport/src/runtime/widget.tsx
+++ widgets/excelExport/src/runtime/widget.tsx
@@ -1,10 +1,27 @@
 import { React, AllWidgetProps } from 'jimu-core';
 import defaultMessages from './translations/default';
 
+import XLSX from 'xlsx';
+
 export default class Widget extends React.PureComponent<AllWidgetProps<unknown>> {
     render() {
         const features = this.props?.mutableStateProps?.features;
-        console.log('Excel Export render', features);
+        console.log('Excel Export render', this.props?.mutableStateProps?.label, ' | ', features);
+
+        // TODO: Automatic export writes 2 files most of the times.. maybe because 2 props are set and reder() es executed 2 times?
+        if (features?.length > 0) {
+            const label =
+                this.props?.mutableStateProps?.label?.length > 0
+                    ? this.props?.mutableStateProps?.label
+                    : defaultMessages._widgetLabel;
+            const filename = label.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+            const sheetname = label.substr(0, 31);
+            const ws = XLSX.utils.json_to_sheet(features, { sheet: sheetname });
+            const wb = XLSX.utils.book_new();
+            XLSX.utils.book_append_sheet(wb, ws, sheetname);
+            XLSX.writeFile(wb, `${filename}.xlsb`);
+        }
+
         return (
             <div>
                 <h3>{defaultMessages._widgetLabel}</h3>
