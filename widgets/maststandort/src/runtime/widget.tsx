3bef58255ee953d6fe05aeff3594ed6360957164
Niklas Köhn <n.koehn@esri.de>
2022-03-22 23:48:56 +0100

    geodesicbuffer

--- widgets/maststandort/src/runtime/widget.tsx
+++ widgets/maststandort/src/runtime/widget.tsx
@@ -11,6 +11,10 @@ import Graphic from 'esri/Graphic';
 import PictureMarkerSymbol from 'esri/symbols/PictureMarkerSymbol';
 import Polygon from 'esri/geometry/Polygon';
 import { NumericInput } from 'jimu-ui';
+import LineSymbolMarker from 'esri/symbols/LineSymbolMarker';
+import { Polyline } from 'esri/geometry';
+import geometryEngine from 'esri/geometry/geometryEngine';
+import SpatialReference from 'esri/geometry/SpatialReference';
 interface State {
     center: __esri.Point;
     angle: number;
@@ -20,10 +24,20 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
     mastStandortLayer: GraphicsLayer;
 
     state: State = {
-        center: null,
-        angle: null,
+        center: new Point({
+            x: 8.6,
+            y: 52.4,
+            spatialReference: SpatialReference.WGS84
+        }),
+        angle: 20,
     };
+
+    // state: State = {
+    //     center: null,
+    //     angle: null,
+    // };
     mapView: __esri.MapView | __esri.SceneView;
+    helperLayer: GraphicsLayer;
 
     constructor(props: any) {
         super(props);
@@ -37,6 +51,9 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
         this.mastStandortLayer = new GraphicsLayer({
             listMode: 'hide',
         });
+        this.helperLayer = new GraphicsLayer({
+            listMode: 'hide',
+        });
     }
 
     // handleMapClick = (mapClick: any) => {
@@ -48,27 +65,118 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
     //     console.log('pointGraphic', pointGraphic);
     // };
 
+
+    getArrowSym() {
+        return new LineSymbolMarker({
+            color: "blue",
+            placement: "begin-end",
+            style: "arrow"
+        });
+    }
+
+    getPointSym() {
+        return {
+            type: "simple-marker",  // autocasts as new SimpleMarkerSymbol()
+            style: "square",
+            color: "blue",
+            size: "8px",  // pixels
+            outline: {  // autocasts as new SimpleLineSymbol()
+                color: [255, 255, 0],
+                width: 3  // points
+            }
+        }
+    }
+
+    getFillSym() {
+        return {
+            type: "simple-fill",  // autocasts as new SimpleFillSymbol()
+            color: [51, 51, 204, 0.9],
+            style: "solid",
+            outline: {  // autocasts as new SimpleLineSymbol()
+                color: "white",
+                width: 1
+            }
+        };
+    }
+
     drawMast = () => {
         if (!this.state.angle || !this.state.center) return;
         // jimuMapView.view.on('click', this.handleMapClick);
 
-        this.mapView.goTo(this.state.center);
+        const wmCenter = webMercatorUtils.geographicToWebMercator(this.state.center);
+        // const offsetWmCenter = geometryEngine.offset(wmCenter, this.props.config.radiusKm, 'kilometers');
 
-        const logoSym = {
-            type: 'picture-marker',
-            url:
-                'data:image/svg+xml;base64,' +
-                'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNDYuMjcyNSAxNDYuMjcyNSI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiNlMTFmMjY7fS5jbHMtMntmaWxsOiNmZmY7fS5jbHMtM3tmaWxsOm5vbmU7fTwvc3R5bGU+PC9kZWZzPjxnIGlkPSJMYXllcl8yIiBkYXRhLW5hbWU9IkxheWVyIDIiPjxnIGlkPSJhcnR3b3JrIj48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0xMTcuMDIwNiwyOS4yNTIySDI5LjI1NDl2ODcuNzY1Nmg4Ny43NjU3VjI5LjI1MjJaIi8+PHBhdGggY2xhc3M9ImNscy0yIiBkPSJNNjcuNjUyNSw5Mi4zMzQ2YTIuNzQ1NSwyLjc0NTUsMCwwLDEtMi42MDItMy42MUw3Ni4wMjEyLDU1LjgxMmEyLjc0MjksMi43NDI5LDAsMCwxLDUuMjA0MSwxLjczNTZMNzAuMjU0Niw5MC40NkEyLjc0MjksMi43NDI5LDAsMCwxLDY3LjY1MjUsOTIuMzM0NloiLz48cGF0aCBjbGFzcz0iY2xzLTIiIGQ9Ik01MS4xOTY1LDkyLjMzNDZhMi43NDU2LDIuNzQ1NiwwLDAsMS0yLjYwMjEtMy42MUw1OS41NjUxLDU1LjgxMmEyLjc0MywyLjc0MywwLDAsMSw1LjIwNDIsMS43MzU2TDUzLjc5ODUsOTAuNDZBMi43NDI5LDIuNzQyOSwwLDAsMSw1MS4xOTY1LDkyLjMzNDZaIi8+PHBhdGggY2xhc3M9ImNscy0yIiBkPSJNODQuMTA4Niw5Mi4zMzQ2YTIuNzQ1NiwyLjc0NTYsMCwwLDEtMi42MDIxLTMuNjFMOTIuNDc3Miw1NS44MTJhMi43NDMsMi43NDMsMCwwLDEsNS4yMDQyLDEuNzM1Nkw4Ni43MTA3LDkwLjQ2QTIuNzQzLDIuNzQzLDAsMCwxLDg0LjEwODYsOTIuMzM0NloiLz48cmVjdCBjbGFzcz0iY2xzLTMiIHdpZHRoPSIxNDYuMjcyNSIgaGVpZ2h0PSIxNDYuMjcyNSIvPjwvZz48L2c+PC9zdmc+',
-            contentType: 'image/svg',
-            width: 25,
-            height: 25,
-        } as unknown as PictureMarkerSymbol;
-        const w3wlogo = new Graphic({
-            geometry: this.state.center,
-            symbol: logoSym,
-        });
-        this.mastStandortLayer.graphics.add(w3wlogo);
-        this.mapView.map.add(this.mastStandortLayer);
+        const distBufferRadius = geometryEngine.geodesicBuffer(wmCenter, this.props.config.radiusKm, 'kilometers') as Polygon;
+        // const distBufferRadius = geometryEngine.buffer(wmCenter, this.props.config.radiusKm, 'kilometers') as Polygon;
+        // const distPointRadius = geometryEngine.offset(this.state.center, this.props.config.radiusKm) as Point;
+        const offsetWmCenter = geometryEngine.offset(distBufferRadius, this.props.config.radiusKm, 'kilometers');
+
+        // First create a line geometry (this is the Keystone pipeline)
+        const cutline = {
+            type: "polyline", // autocasts as new Polyline()
+            paths: [
+                [this.state.center.x, this.state.center.y],
+                [this.state.center.x, 90],
+            ],
+            spatialReference: SpatialReference.WGS84
+        } as unknown as Polyline;
+        const outerPoint = geometryEngine.intersect(distBufferRadius, cutline) as Point;
+
+        this.helperLayer.graphics.addMany([
+            new Graphic({
+                geometry: this.state.center,
+                symbol: this.getPointSym()
+            }),
+            new Graphic({
+                geometry: offsetWmCenter,
+                symbol: this.getPointSym()
+            }),
+            new Graphic({
+                geometry: cutline,
+                symbol: this.getArrowSym()
+            }),
+            new Graphic({
+                geometry: distBufferRadius,
+                symbol: this.getFillSym()
+            }),
+        ])
+        
+        this.mapView.goTo(distBufferRadius);
+
+        // // First create a line geometry (this is the Keystone pipeline)
+        // const polyline = {
+        //     type: "polyline", // autocasts as new Polyline()
+        //     paths: [
+        //         [this.state.center.x, this.state.center.y],
+        //         [outerPoint.x, outerPoint.y],
+        //     ],
+        //     spatialReference: SpatialReference.WGS84
+        // } as unknown as Polyline;
+
+
+        // // const logoSym = {
+        // //     type: 'picture-marker',
+        // //     url:
+        // //         'data:image/svg+xml;base64,' +
+        // //         'PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNDYuMjcyNSAxNDYuMjcyNSI+PGRlZnM+PHN0eWxlPi5jbHMtMXtmaWxsOiNlMTFmMjY7fS5jbHMtMntmaWxsOiNmZmY7fS5jbHMtM3tmaWxsOm5vbmU7fTwvc3R5bGU+PC9kZWZzPjxnIGlkPSJMYXllcl8yIiBkYXRhLW5hbWU9IkxheWVyIDIiPjxnIGlkPSJhcnR3b3JrIj48cGF0aCBjbGFzcz0iY2xzLTEiIGQ9Ik0xMTcuMDIwNiwyOS4yNTIySDI5LjI1NDl2ODcuNzY1Nmg4Ny43NjU3VjI5LjI1MjJaIi8+PHBhdGggY2xhc3M9ImNscy0yIiBkPSJNNjcuNjUyNSw5Mi4zMzQ2YTIuNzQ1NSwyLjc0NTUsMCwwLDEtMi42MDItMy42MUw3Ni4wMjEyLDU1LjgxMmEyLjc0MjksMi43NDI5LDAsMCwxLDUuMjA0MSwxLjczNTZMNzAuMjU0Niw5MC40NkEyLjc0MjksMi43NDI5LDAsMCwxLDY3LjY1MjUsOTIuMzM0NloiLz48cGF0aCBjbGFzcz0iY2xzLTIiIGQ9Ik01MS4xOTY1LDkyLjMzNDZhMi43NDU2LDIuNzQ1NiwwLDAsMS0yLjYwMjEtMy42MUw1OS41NjUxLDU1LjgxMmEyLjc0MywyLjc0MywwLDAsMSw1LjIwNDIsMS43MzU2TDUzLjc5ODUsOTAuNDZBMi43NDI5LDIuNzQyOSwwLDAsMSw1MS4xOTY1LDkyLjMzNDZaIi8+PHBhdGggY2xhc3M9ImNscy0yIiBkPSJNODQuMTA4Niw5Mi4zMzQ2YTIuNzQ1NiwyLjc0NTYsMCwwLDEtMi42MDIxLTMuNjFMOTIuNDc3Miw1NS44MTJhMi43NDMsMi43NDMsMCwwLDEsNS4yMDQyLDEuNzM1Nkw4Ni43MTA3LDkwLjQ2QTIuNzQzLDIuNzQzLDAsMCwxLDg0LjEwODYsOTIuMzM0NloiLz48cmVjdCBjbGFzcz0iY2xzLTMiIHdpZHRoPSIxNDYuMjcyNSIgaGVpZ2h0PSIxNDYuMjcyNSIvPjwvZz48L2c+PC9zdmc+',
+        // //     contentType: 'image/svg',
+        // //     width: 25,
+        // //     height: 25,
+        // // } as unknown as PictureMarkerSymbol;
+        // const hslPointArrow = new Graphic({
+        //     geometry: polyline,
+        //     symbol: this.getArrowSym() //logoSym,
+        // });
+        // console.log('arrowSym', hslPointArrow);
+        // this.mastStandortLayer.graphics.add(hslPointArrow);
+        // this.mapView.map.add(this.mastStandortLayer);
+
+
+
+
+
+
+        this.mapView.map.add(this.helperLayer);
 
 
         // } else {
