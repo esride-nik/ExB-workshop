6d148e260c49e731ddc56fa4e9b1c2dda21533e0
Niklas Köhn <n.koehn@esri.de>
2022-03-23 15:24:06 +0100

    sample points

--- widgets/maststandort/src/runtime/widget.tsx
+++ widgets/maststandort/src/runtime/widget.tsx
@@ -15,6 +15,7 @@ import geometryEngine from 'esri/geometry/geometryEngine';
 import SpatialReference from 'esri/geometry/SpatialReference';
 import SimpleLineSymbol from 'esri/symbols/SimpleLineSymbol';
 import Color from 'esri/Color';
+import SimpleMarkerSymbol from 'esri/symbols/SimpleMarkerSymbol';
 interface State {
     x: number;
     y: number;
@@ -79,18 +80,18 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
         }
     }
 
-    // getPointSym() {
-    //     return {
-    //         type: "simple-marker",
-    //         style: "square",
-    //         color: "blue",
-    //         size: "8px",  // pixels
-    //         outline: {
-    //             color: [255, 255, 0],
-    //             width: 3  // points
-    //         }
-    //     }
-    // }
+    getPointSym(size: number) {
+        return {
+            type: "simple-marker",
+            style: "square",
+            color: "blue",
+            size: size/4 + "px",  // pixels
+            outline: {
+                color: [255, 255, 0],
+                width: 3  // points
+            }
+        } as unknown as SimpleMarkerSymbol;
+    }
 
     getPolySym() {
         const fillColor = new Color(this.state.color);
@@ -153,13 +154,18 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
             spatialReference: wmCenter.spatialReference
         })
 
+
+
         const anglePolygonRotated = geometryEngine.rotate(anglePolygon, -this.state.angle, wmCenter);
+
+        const sampleGraphics = this.getSamplePoints()
 
         this.mastStandortLayer.graphics.addMany([
             new Graphic({
                 geometry: anglePolygonRotated,
                 symbol: this.getPolySym()
             }),
+            ...sampleGraphics,
             new Graphic({
                 geometry: wmCenter,
                 symbol: this.getArrowMarkerSym()
@@ -207,6 +213,15 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
         this.setState({
             color: colorPicker?.target?.value
         }, this.checkInputValidity);
+    }
+
+    private getSamplePoints() {
+        return this.sampleFeatures.map((sampleFeature: any) => {
+            sampleFeature.geometry.spatialReference = SpatialReference.WebMercator;
+            sampleFeature.geometry.type = 'point';
+            sampleFeature.symbol = this.getPointSym(parseInt(sampleFeature.attributes["signal"]) * -1);
+            return sampleFeature as unknown as Graphic;
+        });
     }
 
     render() {
@@ -265,4 +280,180 @@ export default class Widget extends BaseWidget<AllWidgetProps<IMConfig>, State> 
             </div>
         );
     }
+
+
+
+    sampleFeatures = [
+        {
+          "attributes" : {
+            "OBJECTID" : 1, 
+            "id" : 29070514, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5795, 
+            "geo_e" : 7.6843, 
+            "signal" : -82, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -73, 
+            "signal_quality" : 10
+          }, 
+          "geometry" : 
+          {
+            "x" : 855417.63185425557, 
+            "y" : 6373763.3239450436
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 2, 
+            "id" : 29070555, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5791, 
+            "geo_e" : 7.685, 
+            "signal" : -83, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -67, 
+            "signal_quality" : 17
+          }, 
+          "geometry" : 
+          {
+            "x" : 855484.8585853033, 
+            "y" : 6373694.0121365078
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 3, 
+            "id" : 29070576, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5791, 
+            "geo_e" : 7.6856, 
+            "signal" : -81, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -70, 
+            "signal_quality" : 22
+          }, 
+          "geometry" : 
+          {
+            "x" : 855561.63574942283, 
+            "y" : 6373693.39871009
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 4, 
+            "id" : 29070755, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5783, 
+            "geo_e" : 7.6874, 
+            "signal" : -88, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -74, 
+            "signal_quality" : 6
+          }, 
+          "geometry" : 
+          {
+            "x" : 855762.883466343, 
+            "y" : 6373557.1190677416
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 5, 
+            "id" : 29070804, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5783, 
+            "geo_e" : 7.6881, 
+            "signal" : -87, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -75, 
+            "signal_quality" : 0
+          }, 
+          "geometry" : 
+          {
+            "x" : 855831.32079685177, 
+            "y" : 6373556.0170471165
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 6, 
+            "id" : 29070853, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5787, 
+            "geo_e" : 7.6869, 
+            "signal" : -85, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -70, 
+            "signal_quality" : 0
+          }, 
+          "geometry" : 
+          {
+            "x" : 855697.66059744917, 
+            "y" : 6373625.9644842539
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 7, 
+            "id" : 29071158, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5783, 
+            "geo_e" : 7.6807, 
+            "signal" : -74, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -66, 
+            "signal_quality" : 0
+          }, 
+          "geometry" : 
+          {
+            "x" : 855006.71610282164, 
+            "y" : 6373556.946528282
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 8, 
+            "id" : 29071205, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5779, 
+            "geo_e" : 7.68, 
+            "signal" : -74, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -68, 
+            "signal_quality" : 6
+          }, 
+          "geometry" : 
+          {
+            "x" : 854931.46691003512, 
+            "y" : 6373486.9321972737
+          }
+        }, 
+        {
+          "attributes" : {
+            "OBJECTID" : 9, 
+            "id" : 29071245, 
+            "timestamp" : 1578556800000, 
+            "geo_n" : 49.5775, 
+            "geo_e" : 7.68, 
+            "signal" : -74, 
+            "fkcell" : 75648, 
+            "source_id" : 1, 
+            "max_sig" : -64, 
+            "signal_quality" : 0
+          }, 
+          "geometry" : 
+          {
+            "x" : 854929.64817219193, 
+            "y" : 6373420.0323508233
+          }
+        }
+      ]
 }
