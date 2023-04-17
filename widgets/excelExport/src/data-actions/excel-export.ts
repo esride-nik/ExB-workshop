4b5593e03d5e40b5f81bc8c297959008947ecd4f
Niklas Köhn <n.koehn@esri.de>
2023-04-17 19:36:06 +0200

    ExcelExport: breaking changes fixed

--- widgets/excelExport/src/data-actions/excel-export.ts
+++ widgets/excelExport/src/data-actions/excel-export.ts
@@ -1,21 +1,22 @@
-import * as Graphic from 'esri/Graphic'
-import { AbstractDataAction, DataSource, DataRecord, MutableStoreManager } from 'jimu-core'
+import * as Graphic from 'esri/Graphic';
+import { AbstractDataAction, DataSource, DataRecord, MutableStoreManager, DataRecordSet } from 'jimu-core';
 
 export default class ExportJson extends AbstractDataAction {
-  async isSupported (dataSource: DataSource, records: DataRecord[]): Promise<boolean> {
-    return records?.length > 0
-  }
-
-  // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
-  async onExecute (dataSource: DataSource, records: DataRecord[]): Promise<boolean> {
-    if (records.length > 0) {
-      const features = records.map((r) => (r as any).feature as Graphic)
-      MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'results', {
-        features: features,
-        label: records[0].dataSource?.belongToDataSource?.fetchedSchema?.label
-      })
-      return true
+    async isSupported(dataSet: DataRecordSet): Promise<boolean> {
+        return dataSet.records?.length > 0;
     }
-    return false
-  }
+
+    // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
+    async onExecute(dataSet: DataRecordSet, actionConfig: any): Promise<boolean> {
+        if (dataSet.records.length > 0) {
+            const features = dataSet.records.map((r) => (r as any).feature as Graphic);
+            MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'results', {
+                features: features,
+                label: dataSet.records[0].dataSource?.belongToDataSource?.fetchedSchema?.label,
+            });
+            return true;
+        }
+        return false;
+    }
 }
+
