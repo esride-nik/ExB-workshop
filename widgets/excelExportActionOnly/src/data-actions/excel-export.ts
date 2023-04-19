aa1affa26fa71e4f10d4e4b84e6e2f8cd299b02b
Niklas Köhn <n.koehn@esri.de>
2023-04-19 14:04:24 +0200

    excelExportActionOnly init

---
+++ widgets/excelExportActionOnly/src/data-actions/excel-export.ts
@@ -0,0 +1,22 @@
+import Graphic from 'esri/Graphic'
+import { AbstractDataAction, MutableStoreManager, DataRecordSet } from 'jimu-core'
+import { JSON2SheetOpts, utils, writeFile } from 'xlsx'
+
+export default class ExportJson extends AbstractDataAction {
+  async isSupported (dataSet: DataRecordSet): Promise<boolean> {
+    return dataSet.records?.length > 0
+  }
+
+  // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
+  async onExecute (dataSet: DataRecordSet, actionConfig: any): Promise<boolean> {
+    if (dataSet.records.length > 0) {
+      const features = dataSet.records.map((r) => (r as any).feature as Graphic)
+      MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'results', {
+        features: features,
+        label: dataSet.records[0].dataSource?.belongToDataSource?.fetchedSchema?.label
+      })
+      return true
+    }
+    return false
+  }
+}
---
+++ widgets/excelExportActionOnly/src/runtime/translations/default.ts
@@ -0,0 +1,8 @@
+export default {
+  _widgetLabel: 'Excel Action',
+  queryRelationships: 'Query related tables',
+  export2Excel: 'Export to Excel',
+  recordsReceived: 'records received.',
+  relationshipsDetected: 'relationships detected.',
+  noRecords: 'No records to display.',
+}
