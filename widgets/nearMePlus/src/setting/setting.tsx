b728d309fc101ba8461eaa7aad02864154648989
Niklas Köhn <n.koehn@esri.de>
2023-09-26 09:55:17 +0200

    lint

--- widgets/nearMePlus/src/setting/setting.tsx
+++ widgets/nearMePlus/src/setting/setting.tsx
@@ -17,22 +17,22 @@
   A copy of the license is available in the repository's
   LICENSE file.
 */
-import { React } from 'jimu-core';
-import { MapWidgetSelector } from 'jimu-ui/advanced/setting-components';
-import { AllWidgetSettingProps } from 'jimu-for-builder';
+import { React } from 'jimu-core'
+import { MapWidgetSelector } from 'jimu-ui/advanced/setting-components'
+import { type AllWidgetSettingProps } from 'jimu-for-builder'
 
-export default function Setting(
+export default function Setting (
   props: AllWidgetSettingProps<{}>
 ): React.ReactElement {
-
   const onMapSelected = (useMapWidgetIds: string[]) => {
     props.onSettingChange({
       id: props.id,
       useMapWidgetIds: useMapWidgetIds
-    });
+    })
   }
 
   return <div className="sample-js-api-widget-setting p-2">
     <MapWidgetSelector onSelect={onMapSelected} useMapWidgetIds={props.useMapWidgetIds} />
+    // TODO: add layer selector, use layerId in widget.tsx
   </div>
 }
