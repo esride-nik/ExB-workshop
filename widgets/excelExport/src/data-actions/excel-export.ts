4981f0bea9b4dab3424346287e6ac8aa57ca5d4e
Niklas Köhn <n.koehn@esri.de>
2024-01-23 20:54:29 +0100

    fixed breaking changes in ExB 1.13

--- widgets/excelExport/src/data-actions/excel-export.ts
+++ widgets/excelExport/src/data-actions/excel-export.ts
@@ -1,18 +1,22 @@
 import type Graphic from 'esri/Graphic'
-import { AbstractDataAction, MutableStoreManager, type DataRecordSet } from 'jimu-core'
+import { AbstractDataAction, MutableStoreManager, type DataRecordSet, type DataLevel, type DataRecord } from 'jimu-core'
 
 export default class ExportJson extends AbstractDataAction {
-  async isSupported (dataSet: DataRecordSet): Promise<boolean> {
-    return dataSet.records?.length > 0
+  // changed according to breaking changes in ExB 1.13: https://developers.arcgis.com/experience-builder/guide/whats-new/#data-action [2024-01-23]
+  async isSupported (dataSets: DataRecordSet[], dataLevel: DataLevel, widgetId: string): Promise<boolean> {
+    console.log('ExportJson.isSupported', dataSets, dataLevel, widgetId)
+    return dataSets.length > 0
   }
 
   // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
-  async onExecute (dataSet: DataRecordSet, actionConfig: any): Promise<boolean> {
-    if (dataSet.records.length > 0) {
-      const features = dataSet.records.map((r) => (r as any).feature as Graphic)
+  async onExecute (dataSets: DataRecordSet[], dataLevel: DataLevel, widgetId: string): Promise<boolean> {
+    console.log('ExportJson.onExecute', dataSets, dataLevel, widgetId)
+    if (dataSets.length > 0) {
+      // TODO: The following "as any" casts are due to insufficient typings in ExB 1.13. Re-check in future versions.
+      const features = dataSets.map((drs: DataRecordSet) => drs.records.map((r: DataRecord) => (r as any).feature as Graphic)).flat()
       MutableStoreManager.getInstance().updateStateValue(this.widgetId, 'results', {
         features: features,
-        label: dataSet.records[0].dataSource?.fetchedSchema?.label
+        label: (dataSets[0].dataSource as any)?.fetchedSchema?.label
       })
       return true
     }
