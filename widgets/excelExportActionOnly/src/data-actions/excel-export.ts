b20afcb54d92d95cd0d0d339f652f4f87eddef4d
Niklas Köhn <n.koehn@esri.de>
2024-01-24 18:30:30 +0100

    excel actions records check fixed

--- widgets/excelExportActionOnly/src/data-actions/excel-export.ts
+++ widgets/excelExportActionOnly/src/data-actions/excel-export.ts
@@ -12,13 +12,13 @@ export default class ExportJson extends AbstractDataAction {
   // changed according to breaking changes in ExB 1.13: https://developers.arcgis.com/experience-builder/guide/whats-new/#data-action [2024-01-23]
   async isSupported (dataSets: DataRecordSet[], dataLevel: DataLevel, widgetId: string): Promise<boolean> {
     console.log('ExportJson.isSupported', dataSets, dataLevel, widgetId)
-    return dataSets.length > 0
+    return dataSets[0]?.records.length > 0
   }
 
   // TODO: Hidden columns are exported as well. Can we see which ones are hidden in the datasource?
   async onExecute (dataSets: DataRecordSet[], dataLevel: DataLevel, widgetId: string): Promise<boolean> {
     console.log('ExportJson.onExecute', dataSets, dataLevel, widgetId)
-    if (dataSets.length > 0) {
+    if (dataSets[0]?.records.length > 0) {
       // TODO: The following "as any" casts are due to insufficient typings in ExB 1.13. Re-check in future versions.
       const features = dataSets.map((drs: DataRecordSet) => drs.records.map((r: DataRecord) => (r as any).feature as Graphic)).flat()
       const label = (dataSets[0].dataSource as any)?.fetchedSchema?.label.length > 0
