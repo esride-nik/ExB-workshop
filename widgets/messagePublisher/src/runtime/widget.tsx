889fd6a8c9a4c8d1ba3eb4e430c1cfd201e45f78
Niklas Köhn <n.koehn@esri.de>
2024-02-22 09:16:08 +0100

    dummy change

--- widgets/messagePublisher/src/runtime/widget.tsx
+++ widgets/messagePublisher/src/runtime/widget.tsx
@@ -39,7 +39,7 @@ export default function Widget (props: AllWidgetProps<{ Config }>) {
       where: '1=1',
       outFields: [field],
       orderByFields: [`${field} DESC`],
-      pageSize: 3,
+      pageSize: 5,
       page: 1,
       returnGeometry: true
     })
