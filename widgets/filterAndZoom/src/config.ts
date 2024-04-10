a8b564b47cc94c28b572620f2ccd7d999cc5f8ac
Niklas Köhn <n.koehn@esri.de>
2024-04-10 09:19:00 +0200

    filterAndZoom widget: read URL + get dataSource

---
+++ widgets/filterAndZoom/src/config.ts
@@ -0,0 +1,6 @@
+import { ImmutableObject } from 'jimu-core'
+
+export interface Config {
+}
+
+export type IMConfig = ImmutableObject<Config>
---
+++ widgets/filterAndZoom/src/runtime/widget.tsx
@@ -0,0 +1,56 @@
+import { DataSourceManager, React, type AllWidgetProps } from 'jimu-core'
+import { JimuMapViewComponent, type JimuMapView } from 'jimu-arcgis'
+import Graphic from 'esri/Graphic'
+import { type Point } from 'esri/geometry'
+
+const { useState, useEffect } = React
+
+/**
+ * This widget will show features from a configured feature layer
+ */
+export default function Widget (props: AllWidgetProps<{ Config }>) {
+  const [dataSourceId, setDataSourceId] = useState<string>(undefined)
+
+  useEffect(() => {
+    // queryFunc();
+  }, [])
+
+  let jimuMapView: JimuMapView
+
+  const isConfigured = () => {
+    return props.useMapWidgetIds && props.useMapWidgetIds.length === 1
+  }
+
+  const activeViewChangeHandler = (jmv: JimuMapView) => {
+    jimuMapView = jmv
+
+    const queryString = window.location.search
+    const urlParams = new URLSearchParams(queryString)
+    console.log('urlParams', urlParams.get('data_filter'))
+    const dataFilter = urlParams.get('data_filter')
+    const dataSourceId = dataFilter.split(':')[0]
+    const whereClause = dataFilter.split(':')[1]
+    console.log('dataSourceId', dataSourceId, whereClause)
+    console.log('whereClause', whereClause)
+
+    // find datasource from DataSourceManager by ID from data_filter
+    const dataSource = DataSourceManager.getInstance().getDataSource(dataSourceId)
+    console.log('dataSource', dataSource)
+  }
+
+  if (!isConfigured()) {
+    return 'Select a map'
+  }
+  return (
+        <div
+            className="widget-map-click"
+            style={{ width: '100%', height: '100%', maxHeight: '800px', overflow: 'auto' }}>
+            {{}.hasOwnProperty.call(props, 'useMapWidgetIds') && props.useMapWidgetIds && props.useMapWidgetIds.length === 1 && (
+                <JimuMapViewComponent
+                    useMapWidgetId={props.useMapWidgetIds?.[0]}
+                    onActiveViewChange={activeViewChangeHandler}
+                />
+            )}
+        </div>
+  )
+}
