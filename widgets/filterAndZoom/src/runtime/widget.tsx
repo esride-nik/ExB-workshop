fe0cc875241728147931fa5bda2217a0b0819ad7
Niklas Köhn <n.koehn@esri.de>
2025-02-06 12:19:19 +0100

    extent as graphic

--- widgets/filterAndZoom/src/runtime/widget.tsx
+++ widgets/filterAndZoom/src/runtime/widget.tsx
@@ -1,5 +1,7 @@
 import { DataSourceManager, React, type AllWidgetProps } from 'jimu-core'
 import { type FeatureLayerDataSource, JimuMapViewComponent, type JimuMapView } from 'jimu-arcgis'
+import Graphic from 'esri/Graphic'
+import SpatialReference from 'esri/geometry/SpatialReference'
 
 /**
  * This widget will show features from a configured feature layer
@@ -32,7 +34,33 @@ export default function Widget (props: AllWidgetProps<{ Config }>) {
     const flQuery = featureLayerDataSource.layer.createQuery()
     flQuery.where = whereClause
     const flFilteredExtent = await featureLayerDataSource.layer.queryExtent(flQuery)
-    console.log('flFilteredExtent', flFilteredExtent)
+    console.log('flFilteredExtent', flFilteredExtent, flFilteredExtent.extent.xmax, flFilteredExtent.extent.ymax)
+
+    jimuMapView.view.graphics.removeAll()
+    const polygon = {
+      type: "polygon",
+      rings: [
+        [flFilteredExtent.extent.xmax, flFilteredExtent.extent.ymax],
+        [flFilteredExtent.extent.xmax, flFilteredExtent.extent.ymin],
+        [flFilteredExtent.extent.xmin, flFilteredExtent.extent.ymin],
+        [flFilteredExtent.extent.xmin, flFilteredExtent.extent.ymax],
+        [flFilteredExtent.extent.xmax, flFilteredExtent.extent.ymax]
+      ],
+      spatialReference: flFilteredExtent.extent
+    } as unknown as __esri.Polygon;
+    const fillSymbol = {
+      type: "simple-fill",
+      color: [227, 139, 79, 0.8],
+      outline: {
+        color: [255, 255, 255],
+        width: 2
+      }
+    };
+    const polygonGraphic = new Graphic({
+      geometry: polygon,
+      symbol: fillSymbol
+    });
+    jimuMapView.view.graphics.add(polygonGraphic);
 
     jimuMapView.view.goTo(flFilteredExtent)
   }
