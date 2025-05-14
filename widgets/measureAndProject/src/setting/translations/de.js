6d9d80296581489aaf3630c6bf5dc476c1284e89
Niklas Köhn <n.koehn@esri.de>
2025-05-14 16:43:22 +0200

    settings: translations

--- widgets/measureAndProject/src/setting/translations/de.js
+++ widgets/measureAndProject/src/setting/translations/de.js
@@ -1,3 +1,12 @@
 System.register([], function (_export) {return {execute: function () {_export({
-  selectMapWidget: 'Karten-Widget wählen'
+  selectMapWidget: 'Karten-Widget wählen',
+  headerMeterValues: 'Ausgabe der Meter-Werte für Linien- und Flächenmessung',
+  decimalPlaces: 'Dezimalstellen',
+  decimalPlacesRounded: 'Dezimalstellen gerundet auf 0,5m',
+  noDecimalPlaces: 'Keine Dezimalstellen',
+  description: 'Beschreibung',
+  measurementTypes: 'Messarten',
+  area: 'Fläche',
+  distance: 'Entfernung',
+  location: 'Ort'
 })}}});
--- widgets/measureAndProject/src/setting/translations/default.ts
+++ widgets/measureAndProject/src/setting/translations/default.ts
@@ -1,3 +1,12 @@
 export default {
-  selectMapWidget: 'Select map widget'
+  selectMapWidget: 'Select map widget',
+  headerMeterValues: 'Meter value output for distance and area measurement',
+  decimalPlaces: 'Decimal places',
+  decimalPlacesRounded: 'Decimal places rounded to 0.5m',
+  noDecimalPlaces: 'No decimal places',
+  description: 'Description',
+  measurementTypes: 'Measurement types',
+  area: 'Area',
+  distance: 'Distance',
+  location: 'Location'
 }
